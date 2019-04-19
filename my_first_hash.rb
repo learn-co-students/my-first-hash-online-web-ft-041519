7ad0c01ced885e77690db0308820859c27cff75c
Erin Stalzer <erinstalzer@gmail.com>
2019-04-19 13:57:59 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  the_manifest = {"whale bone corsets" => 5, "procelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   puts the_manifest.keys
   puts the_manifest.values
   the_manifest
@@ -41,5 +41,5 @@ def adding
   shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-  puts shipping_manifest
+  shipping_manifest
 end
